295a3bcd38c2021460815bcc7b1b456c828b0a9f
myhuon <myhuon@naver.com>
2021-11-12 16:55:26 +0900

    [Animal] form, controller 수정 & 생성

---
+++ src/main/java/controller/animal/ViewAnimalController.java
@@ -0,0 +1,24 @@
+package controller.animal;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.Controller;
+
+public class ViewAnimalController implements Controller{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		Animal animal = null;
+    	try {
+			animal = manager.findAnimal(animalId);	// 유기동물 정보 검색  
+		} catch (AnimalNotFoundException e) {				
+	        return "redirect:/animal/list";
+		}	
+		
+    	request.setAttribute("animal", animal);		// 유기동물 정보 저장				
+		return "/animal/view.jsp";				// 유기동물 상세보기 화면으로 이동*/
+	}
+
+}
